202b2d27306e58fdf1933d07b499569e4d5c6aca
Tu Hoang <hatu87@gmail.com>
2016-10-03 08:51:33 +0700

    add scss & ajax to calculator#add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
 
+  root to: 'users#index'
   get 'hello', to: 'hello#index'
   get 'calc', to: 'calculator#index'
   post 'calc/add', to: 'calculator#add'
